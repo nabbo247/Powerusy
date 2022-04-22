4e3fb05f18175c8b4539753327c9865dc64ef80b
nabbo <nabbo247@hotmail.com>
2022-04-22 23:56:02 +0100

    update on ifrighter

---
+++ ifreighters/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ifreighters.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult LogOff()
+        {
+            //AuthenticationManager.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ ifreighters/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ifreighters.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult ManageJobs()
+        {
+            return View();
+        }
+        public ActionResult MyProfile()
+        {
+            return View();
+        }
+        
+    }
+}
